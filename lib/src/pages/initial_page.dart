da914bbc0fda93ac02f457882f62ac3cae5796e9
Victor Manuel Gonzalez Del Vasto <vgonzalez@jpglobaldigital.com>
2023-04-04 13:25:26 -0700

    initialPage has been added

---
+++ lib/src/pages/initial_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Initpage extends StatelessWidget {
+  const Initpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+    );
+  }
+}
